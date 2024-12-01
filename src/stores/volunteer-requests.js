64fb4ce5ee1ae05eb472c12ccc56c81a3702524c
ayPromise servant <fatherpother@gmail.com>
2024-12-01 03:41:55 +0200

    store for voluenteer requests

---
+++ src/stores/volunteer-requests.js
@@ -0,0 +1,40 @@
+import { fetchData } from "@/utils/api";
+import { defineStore } from "pinia";
+
+const useRequests = defineStore('requests',{
+    state:()=>({requests:[]}),
+    actions:{
+        async fetchRequests(){
+            if(this.requests.length > 0) return
+                this.requests = await fetchData("http://localhost:5555/api/medhelp/requests")
+        },
+        async initRequests(){
+            await this.fetchRequests()
+        },
+        async getRequestById(id){
+            let request = this.requests.find(r=>r._id === id)
+
+            if(!request){
+                return await fetchData(`http://localhost:5555/api/medhelp/requests/${id}`)
+            }
+
+            return request
+        },
+        getRequestsByAuthor(author){
+            let requests = this.requests.filter(r=>r.author.toLocaleLowerCase().includes(author.toLocaleLowerCase()))
+            return requests 
+        },
+
+        getRequestsByNeedTitle(need){
+            let requests = this.requests.filter(r=>r.needs.some(n=>n.title.toLocaleLowerCase().includes(need.toLocaleLowerCase())))
+            return requests
+        },
+
+        getRequestsByCategory(category){
+            let requests = this.requests.filter(r=>r.category === category)
+            return requests
+        }
+    }
+})
+
+export default useRequests
