b8a00763f3b23e61cb4b7a85b40aec82c39240b8
ayPromise servant <fatherpother@gmail.com>
2024-12-01 05:29:28 +0200

    updated configuration to Vuetify

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import 'vuetify/styles'
 import {aliases, mdi} from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
+import colors from 'vuetify/util/colors'
 
 import useRequests from './stores/volunteer-requests'
 
@@ -16,7 +16,19 @@ import App from './App.vue'
 import router from './router'
 const app = createApp(App)
 const vuetify = createVuetify({
-    components, directives, icons:{defaultSet:'mdi',aliases,sets:{mdi}}
+    components,
+     directives,
+      icons:{defaultSet:'mdi',aliases,sets:{mdi}},
+      theme:{
+        themes:{
+            light:{
+                colors:{
+                    primary: colors.blue.darken4,
+                    secondary:colors.yellow.base
+                }
+            }
+        }
+      }
 })
 app.use(createPinia())
 app.use(vuetify)
---
+++ src/utils/validation/rules.js
@@ -0,0 +1,15 @@
+export const phoneNumberRules = [
+             v => !!v || 'Phone number is required',
+]
+
+export const passwordRules = [
+             v => !!v || 'Password is required',
+]
+
+export const firstNameRules = [
+            v => !!v || 'First name is required',
+]
+
+export const lastNameRules = [
+            v => !!v || 'Last name is required',
+]
