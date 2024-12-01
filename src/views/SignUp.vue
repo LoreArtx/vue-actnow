0b79452c4c4d4fea2b908a73270045fdc82a063d
ayPromise servant <fatherpother@gmail.com>
2024-12-01 05:28:47 +0200

    SignUp view@1.1.1 - Vuetify adapted

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,41 +1,39 @@
 <template>
-    <div class="flex justify-center">
-        <form @submit.prevent="signUp" class="bg-gray-200 w-1/3 p-5 flex flex-col">
-            <h1 class="text-center text-2xl">Welcome</h1>
-            <Input name="firstName" :formVar="firstName" type="text" label="First Name" required/>
+    <v-sheet class="pa-8">
+        <v-card class="mx-auto px-6 py-8 flex" max-width="344">
+            <v-form @submit.prevent="signUp" validate-on="submit lazy">
+                <h1 class="text-center text-2xl mb-2">Welcome</h1>
+                <v-text-field v-model="firstName" :rules="firstNameRules" label="First Name"/>
 
-            <Input name="lastName" :formVar="lastName" type="text" label="Last Name" required/>
+                <v-text-field v-model="lastName" :rules="lastNameRules" label="Last Name"/>
 
-            <Input name="phoneNumber" :formVar="phoneNumber" type="text" label="Phone Number" @input="(event)=>validatePhoneNumber(event)" required/> 
+                <v-text-field v-model="phoneNumber" :rules="phoneNumberRules" label="Phone Number"/> 
 
-            <Input name="password" :formVar="password" type="password" label="Password" required/>
+                <v-text-field v-model="password" :rules="passwordRules" type="password" label="Password"/>
 
-            <Input name="confirmPassword" :formVar="confirmPassword" type="password" label="Confirm Password" required/>
+                <v-text-field v-model="confirmPassword" :rules="passwordRules" type="password" label="Confirm Password"/>
 
-            <RouterLink to="/sign-in" class="text-blue-700 underline mb-3">Already have an account</RouterLink>
 
-            <v-btn class="border border-black">Sign up</v-btn>
-        </form>
-    </div>
+                <br/>
+                <v-btn type="submit" color="primary" block>Sign up</v-btn>
+                <RouterLink to="/sign-in" class="text-blue-700 underline mb-3">Already have an account</RouterLink>
+            </v-form>
+        </v-card>
+    </v-sheet>
 </template>
 
 <script setup>
-    import {reactive} from "vue"
-    import Input from "@/components/UI/Input.vue";
-    const firstName = reactive({value:"", error:""})
-    const lastName = reactive({value:"", error:""})
-    const phoneNumber = reactive({value:"", error:""})
-    const password = reactive({value:"", error:""})
-    const confirmPassword = reactive({value:"", error:""})
+    import {ref} from "vue"
+    import { firstNameRules, lastNameRules, phoneNumberRules, passwordRules } from "@/utils/validation/rules";
+    const firstName = ref('')
+    const lastName = ref('')
+    const phoneNumber = ref('')
+    const password = ref('')
+    const confirmPassword = ref('')
 
     const signUp = async ()=>{
 
-        await checkForErrors()
-
-        if(firstName.error || lastName.error || phoneNumber.error || password.error)
-            return
-
-
+        console.log(12)
         const response = await fetch("http://localhost:5555/api/medhelp/sign-up",
             {
                 method:"POST",
@@ -50,54 +48,6 @@
         console.log(response)
     }
 
-    const checkForErrors = async ()=>{
-        resetErrors()
-
-        const phoneRegex = /^(?:\+38)?(?:\(044\)[ .-]?[0-9]{3}[ .-]?[0-9]{2}[ .-]?[0-9]{2}|044[ .-]?[0-9]{3}[ .-]?[0-9]{2}[ .-]?[0-9]{2}|044[0-9]{7})$/
-
-        if(password.value !== confirmPassword.value )
-            password.error = "Passwords must match"
-
-        if(phoneNumber.value.trim() === '')
-            phoneNumber.error = "Phone number can't be empty"
-
-        if(!phoneRegex.test(phoneNumber.value))
-            phoneNumber.error = "Wrong number"
-
-        if(firstName.value.trim() === "")
-            firstName.error = "First name can't be empty"
-
-        if(lastName.value.trim() === "")
-            lastName.error = "Last name can't be empty"
-
-
-        const userExists = await fetch("http://localhost:5555/api/medhelp/checkNumber?"+ new URLSearchParams({phoneNumber:phoneNumber.value}).toString(),
-            {
-                method:"GET",
-                headers:{
-                    "Accept":"application/json",
-                    "Content-Type":"application/json"
-                },
-                
-            }
-        )
-
-        if(userExists.status === 409)
-            phoneNumber.error = "Such phone number is already used"
-    }
-
-    const resetErrors = ()=>{
-        firstName.error = ""
-        lastName.error = ""
-        phoneNumber.error = ""
-        password.error = ""
-        confirmPassword.error = ""
-    }
-
-    const validatePhoneNumber = (event)=>{
-        phoneNumber.value = event.target.value.replace(/\D+/g, '')
-        phoneNumber.value=phoneNumber.value.slice(0,12);
-    }
 </script>
 
 <style scoped>
