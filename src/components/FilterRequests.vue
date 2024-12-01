1f06a36d8ee4a71dfbd491018af27d12a3211ed3
ayPromise servant <fatherpother@gmail.com>
2024-12-01 05:39:28 +0200

    FilterRequests component @1.0.2 - Vuetify adapted

--- src/components/FilterRequests.vue
+++ src/components/FilterRequests.vue
@@ -1,23 +1,20 @@
 <template>
     <div class="grid grid-cols-8 gap-3">
-        <SearchBar class="col-span-3" :searchValue="organisationName" @input="findByOrganisation" placeholder="Author / Organisation"></SearchBar>
+        <v-text-field class="col-span-3" v-model="organisationName" @update:model-value="findByOrganisation" placeholder="Author / Organisation"></v-text-field>
 
-        <SearchBar class="col-span-3" :searchValue="needTitle" @input="findByNeedTitle" placeholder="Need"></SearchBar>
+        <v-text-field class="col-span-3" v-model="needTitle" @update:model-value="findByNeedTitle" placeholder="Need"></v-text-field>
 
-        <v-select class="col-span-2" :items="selectOptions" label="Category" :formVar="categoryOption" @change="findByCategory"></v-select>
+        <v-select class="col-span-2" :items="selectOptions" label="Category" v-model="categoryOption" @update:model-value="findByCategory"></v-select>
     </div>
 </template>
 
 <script setup>
-    import SearchBar from './UI/SearchBar.vue';
-    import Select from './UI/Select.vue';
-    import { reactive } from 'vue';
+    import { watch, ref } from 'vue';
     import useRequests from '@/stores/volunteer-requests';
-    const organisationName = reactive({value:'', error:''})
-    const needTitle = reactive({value:'', error:''})
-    const categoryOption = reactive({value:'', error:''})
+    const organisationName = ref('')
+    const needTitle = ref('')
+    const categoryOption = ref('All')
     const emit = defineEmits(['filterByAuthor', 'filterByNeedTitle', 'filterByCategory'])
-
     const findByOrganisation = ()=>{
         const {getRequestsByAuthor} = useRequests()
         emit('filterByAuthor',getRequestsByAuthor(organisationName.value),organisationName.value)
@@ -30,13 +27,18 @@
     }
 
     const findByCategory = ()=>{
-        const {getRequestsByCategory} = useRequests()
-        emit('filterByCategory', getRequestsByCategory(categoryOption.value), categoryOption.value)
+        const {getRequestsByCategory, requests} = useRequests()
+        let newRequests = []
+        if(categoryOption.value === 'All')
+            newRequests = requests
+        else newRequests = getRequestsByCategory(categoryOption.value.toLocaleLowerCase())
+
+        emit('filterByCategory', newRequests, categoryOption.value)
     }
 
 
     const selectOptions = [
-        'Military', 'Medical', 'Vehicle', 'Energy', 'Other'
+        'All', 'Military', 'Medical', 'Vehicle', 'Energy', 'Other'
     ];
 </script>
 
